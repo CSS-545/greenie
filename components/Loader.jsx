13fb52e738182dd1807f233276f4b2cf1fa3d9cc
Swanand-Wagh <swanandwagh7@gmail.com>
2024-04-24 00:00:52 -0700

    added Loader component

---
+++ components/Loader.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  View,
+  ActivityIndicator,
+  Dimensions,
+  Platform,
+  StyleSheet,
+} from "react-native";
+
+const Loader = ({ isLoading }) => {
+  const osName = Platform.OS;
+  const screenHeight = Dimensions.get("screen").height;
+
+  if (!isLoading) return null;
+
+  return (
+    <View style={[styles.container, { height: screenHeight }]}>
+      <ActivityIndicator
+        animating={isLoading}
+        color="#fff"
+        size={osName === "ios" ? "large" : 50}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(52, 152, 219, 0.6)",
+    zIndex: 10,
+  },
+});
+
+export default Loader;
