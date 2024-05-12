d8c3ba63b437780706d1bb6698dfb3fae57f6944
Swanand-Wagh <swanandwagh7@gmail.com>
2024-05-11 19:10:08 -0700

    updated readme & added auto logout

--- context/GlobalProvider.jsx
+++ context/GlobalProvider.jsx
@@ -1,10 +1,40 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import { Alert, AppState } from 'react-native';
+import { auth } from '../lib/firebase';
+import { router } from 'expo-router';
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
 
 const GlobalProvider = ({ children }) => {
   const [isLocationVerified, setIsLocationVerified] = useState(false);
+
+  const handleLogout = async () => {
+    await auth
+      .signOut()
+      .then(() => {
+        router.push('/login');
+      })
+      .catch((error) => {
+        Alert.alert(error.message);
+      });
+  };
+
+  // State Management
+  useEffect(() => {
+    const handleAppStateChange = (nextAppState) => {
+      if (nextAppState === 'inactive' || nextAppState === 'paused') {
+        console.log('Logging out user...');
+        handleLogout();
+      }
+    };
+
+    AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, []);
 
   return (
     <GlobalContext.Provider
--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
 ## Checkpoint-3: Greenie
 
 ` Swanand Wagh and Azmeen Kausar`
+
+### Basic Functionality of the Greenie: (things implemented till now):
+- Users can Signup and Login.
+- Users can search for an address & fetch the results from the Google Maps API (address line, city, state, country, postal code, landmark) along with suggested addresses.
+- User can see an interactive map with the fetched address.
+- The address data is stored in a proper format in a reactive state.
+
+### Proper state management  
+- If the state is "inactive" or "paused", the user should not be able to interact & will be logged out.
+
+###  Remaining work to be completed for the next phase of the project ("Beta")
+- Get coordinates of the fetched address (address which user entered).
+- Add a "Capture Location" button to capture the current location of the user using the geolocation API.
+- Write a function to calculate the distance between the two locations (current location and fetched address).
+- If the locations are within a certain distance, display verified location card.
+- If time permits, do the peer-peer work verification.
+
